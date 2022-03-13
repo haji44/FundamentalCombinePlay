8bdec15147935e4b540a0b87295d599f9c4081b6
haji44 <61269325+haji44@users.noreply.github.com>
2022-03-13 16:59:18 +0900

    Chapter3

---
+++ LearningCombine.playground/Pages/UpateUIAdvance.xcplaygroundpage/Contents.swift
@@ -0,0 +1,114 @@
+//: [Previous](@previous)
+
+import Combine
+import PlaygroundSupport
+import UIKit
+
+/*: ## Define new Model
+ */
+
+final class Account {
+    // 変更された値を受け取って通知をおくりたい
+    private let nameSubject: CurrentValueSubject<String, Never>
+    private let ageSubject: CurrentValueSubject<Int, Never>
+
+    // View層とやり取りをする際に使うプロパティー
+    var name: String { nameSubject.value }
+    var age: Int { ageSubject.value }
+
+    // 値を変更を通知したい
+    let isVaild: AnyPublisher<Bool, Never>
+
+    init() {
+        // Initialzie subject value
+        nameSubject = CurrentValueSubject<String, Never>("")
+        ageSubject = CurrentValueSubject<Int, Never>(0)
+        isVaild = nameSubject.combineLatest(ageSubject)
+            .map({ name, age in
+                name.count >= 4 && age >= 18
+            }).eraseToAnyPublisher()
+    }
+
+    func update(userName: String, age: Int) {
+        nameSubject.value = userName
+        ageSubject.value = age
+    }
+}
+
+/*: ## Defeine View and UIComponent
+
+ */
+let view = UIView()
+view.frame = CGRect(x: 0, y: 0, width: 320, height: 160)
+view.backgroundColor = .white
+PlaygroundPage.current.liveView = view
+
+let label = UILabel()
+label.frame = CGRect(x: 20, y: 20, width: 280, height: 20)
+label.textColor = .black
+label.text = "initial text"
+view.addSubview(label)
+
+
+/*: ## Define new receiver
+ */
+
+final class Receiver {
+    private var subscription = Set<AnyCancellable>()
+    private let account = Account()
+
+    init() {
+        account.isVaild
+            .map { "\($0)" }
+            .assign(to: \.text, on: label)
+            .store(in: &subscription)
+    }
+
+    func load(name: String, age: Int) {
+        account.update(userName: name, age: age)
+    }
+}
+
+/*: # Devide responsibility into viewcontroller and viewmodel
+ */
+final class ViewModel {
+    let labelText: AnyPublisher<String? , Never>
+    private var account = Account()
+
+    init() {
+        labelText = account.isVaild
+            .map({ "isValid: \($0)" })
+            .eraseToAnyPublisher()
+    }
+
+    func load(name: String, age: Int) {
+        account.update(userName: name, age: age)
+    }
+}
+final class ViewController {
+    private var subscription = Set<AnyCancellable>()
+    private var viewModel = ViewModel()
+
+    init() {
+        viewModel.labelText
+            .assign(to: \.text, on: label)
+            .store(in: &subscription)
+    }
+
+    func load() {
+        viewModel.load(name: "huge", age: 30)
+    }
+}
+
+
+
+
+let reciver = Receiver()
+reciver.load(name: "hoge", age: 3)
+reciver.load(name: "huga", age: 30)
+
+let vc = ViewController()
+vc.load()
+
+
+//: [Next](@next)
